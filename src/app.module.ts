1b979df32ad9ddf7560c4aca1e524106fa68edb6
Constantine Oganesyants <w.kostjaog@gmail.com>
2023-09-11 21:18:18 +0300

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { DB_HOST } from './core/config';
 
 
 @Module({
   imports: [UsersModule, AuthModule, MongooseModule.forRoot(
-    'mongodb://root:root@localhost:27017/'
+    `mongodb://root:root@${DB_HOST}:27017/`
     )],
   controllers: [AppController],
   providers: [AppService],
